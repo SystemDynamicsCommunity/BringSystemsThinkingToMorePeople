07b8f507520a132ca8b84fee6b9dc757506294af
Yi Lv <yi.lv@odd-e.com>
2018-03-21 15:44:12 +0800

    Update publish_gitbook.sh
    use master to publish

--- publish_gitbook.sh
+++ publish_gitbook.sh
@@ -19,13 +19,12 @@ git config user.email "yi.lv@odd-e.com"
 
 git remote add upstream "https://$GITHUB_TOKEN@github.com/SystemDynamicsCommunity/BringSystemsThinkingToMorePeople.git"
 git fetch upstream
-git reset upstream/gh-pages
 
 touch .
 
 git add -A .
 git commit -m "rebuild pages at $rev"
-git push -q upstream HEAD:gh-pages
+git push -q upstream HEAD
 
 echo "done!"
 
